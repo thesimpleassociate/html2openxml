1daa666dd732d75eac72b1f6a7f1ffa732b1ec23
Chris Maffin <maffin@thesimpleassociate.com>
2022-01-10 14:48:54 -0600

    list indent should add to existing paragraph style

--- src/Html2OpenXml/HtmlDocumentStyle.cs
+++ src/Html2OpenXml/HtmlDocumentStyle.cs
@@ -9,155 +9,189 @@
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
  * PARTICULAR PURPOSE.
  */
-using System;
-using System.Collections.Generic;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
+using System;
 
 namespace HtmlToOpenXml
 {
-	/// <summary>
-	/// Defines the styles to apply on OpenXml elements.
-	/// </summary>
-	public sealed class HtmlDocumentStyle
-	{
-		/// <summary>
-		/// Occurs when a Style is missing in the MainDocumentPart but will be used during the conversion process.
-		/// </summary>
-		public event EventHandler<StyleEventArgs> StyleMissing;
+    /// <summary>
+    /// Defines the styles to apply on OpenXml elements.
+    /// </summary>
+    public sealed class HtmlDocumentStyle
+    {
+        /// <summary>
+        /// Occurs when a Style is missing in the MainDocumentPart but will be used during the conversion process.
+        /// </summary>
+        public event EventHandler<StyleEventArgs> StyleMissing;
 
-		/// <summary>
-		/// Contains the default styles for new OpenXML elements
-		/// </summary>
-		public DefaultStyles DefaultStyles { get { return this.defaultStyles; } }
+        /// <summary>
+        /// Contains the default styles for new OpenXML elements
+        /// </summary>
+        public DefaultStyles DefaultStyles { get { return this.defaultStyles; } }
 
-		private DefaultStyles defaultStyles = new DefaultStyles();
-		private RunStyleCollection runStyle;
-		private TableStyleCollection tableStyle;
-		private ParagraphStyleCollection paraStyle;
+        private DefaultStyles defaultStyles = new DefaultStyles();
+        private RunStyleCollection runStyle;
+        private TableStyleCollection tableStyle;
+        private ParagraphStyleCollection paraStyle;
         private NumberingListStyleCollection listStyle;
-		private OpenXmlDocumentStyleCollection knownStyles;
-		private MainDocumentPart mainPart;
+        private OpenXmlDocumentStyleCollection knownStyles;
+        private MainDocumentPart mainPart;
 
 
-		internal HtmlDocumentStyle(MainDocumentPart mainPart)
-		{
-			PrepareStyles(mainPart);
-			tableStyle = new TableStyleCollection(this);
-			runStyle = new RunStyleCollection(this);
-			paraStyle = new ParagraphStyleCollection(this);
+        internal HtmlDocumentStyle(MainDocumentPart mainPart)
+        {
+            PrepareStyles(mainPart);
+            tableStyle = new TableStyleCollection(this);
+            runStyle = new RunStyleCollection(this);
+            paraStyle = new ParagraphStyleCollection(this);
             this.QuoteCharacters = QuoteChars.IE;
-			this.mainPart = mainPart;
-		}
+            this.mainPart = mainPart;
+        }
 
-		//____________________________________________________________________
-		//
+        //____________________________________________________________________
+        //
 
-		#region PrepareStyles
+        #region PrepareStyles
 
-		/// <summary>
-		/// Preload the styles in the document to match localized style name.
-		/// </summary>
-		internal void PrepareStyles(MainDocumentPart mainPart)
-		{
-			knownStyles = new OpenXmlDocumentStyleCollection();
-			if (mainPart.StyleDefinitionsPart == null) return;
+        /// <summary>
+        /// Preload the styles in the document to match localized style name.
+        /// </summary>
+        internal void PrepareStyles(MainDocumentPart mainPart)
+        {
+            knownStyles = new OpenXmlDocumentStyleCollection();
+            if (mainPart.StyleDefinitionsPart == null) return;
 
-			Styles styles = mainPart.StyleDefinitionsPart.Styles;
+            Styles styles = mainPart.StyleDefinitionsPart.Styles;
 
-			foreach (var s in styles.Elements<Style>())
-			{
-				StyleName n = s.StyleName;
-				if (n != null)
-				{
-					String name = n.Val.Value;
-					if (name != s.StyleId) knownStyles[name] = s;
-				}
+            foreach (var s in styles.Elements<Style>())
+            {
+                StyleName n = s.StyleName;
+                if (n != null)
+                {
+                    String name = n.Val.Value;
+                    if (name != s.StyleId) knownStyles[name] = s;
+                }
 
-				knownStyles.Add(s.StyleId, s);
-			}
-		}
+                knownStyles.Add(s.StyleId, s);
+            }
+        }
 
-		#endregion
+        #endregion
 
-		#region GetStyle
+        #region GetStyle
 
-		/// <summary>
-		/// Helper method to obtain the StyleId of a named style (invariant or localized name).
-		/// </summary>
-		/// <param name="name">The name of the style to look for.</param>
-		/// <param name="styleType">True to obtain the character version of the given style.</param>
-		/// <param name="ignoreCase">Indicate whether the search should be performed with the case-sensitive flag or not.</param>
-		/// <returns>If not found, returns the given name argument.</returns>
-		public String GetStyle(string name, StyleValues styleType = StyleValues.Paragraph, bool ignoreCase = false)
-		{
-			Style style;
+        /// <summary>
+        /// Helper method to obtain the StyleId of a named style (invariant or localized name).
+        /// </summary>
+        /// <param name="name">The name of the style to look for.</param>
+        /// <param name="styleType">True to obtain the character version of the given style.</param>
+        /// <param name="ignoreCase">Indicate whether the search should be performed with the case-sensitive flag or not.</param>
+        /// <returns>If not found, returns the given name argument.</returns>
+        public String GetStyle(string name, StyleValues styleType = StyleValues.Paragraph, bool ignoreCase = false)
+        {
+            Style style;
 
-			// OpenXml is case-sensitive but CSS is not.
-			// We will try to find the styles another time with case-insensitive:
-			if (ignoreCase)
-			{
-				if (!knownStyles.TryGetValueIgnoreCase(name, styleType, out style))
-				{
-					if (StyleMissing != null)
-					{
-						StyleMissing(this, new StyleEventArgs(name, mainPart, styleType));
-						if (knownStyles.TryGetValueIgnoreCase(name, styleType, out style))
-							return style.StyleId;
-					}
-					return null; // null means we ignore this style (css class)
-				}
+            // OpenXml is case-sensitive but CSS is not.
+            // We will try to find the styles another time with case-insensitive:
+            if (ignoreCase)
+            {
+                if (!knownStyles.TryGetValueIgnoreCase(name, styleType, out style))
+                {
+                    if (StyleMissing != null)
+                    {
+                        StyleMissing(this, new StyleEventArgs(name, mainPart, styleType));
+                        if (knownStyles.TryGetValueIgnoreCase(name, styleType, out style))
+                            return style.StyleId;
+                    }
+                    return null; // null means we ignore this style (css class)
+                }
 
-				return style.StyleId;
-			}
-			else
-			{
-				if (!knownStyles.TryGetValue(name, out style))
-				{
-					if (!EnsureKnownStyle(name, out style))
-					{
-						StyleMissing?.Invoke(this, new StyleEventArgs(name, mainPart, styleType));
-						return name;
-					}
-				}
+                return style.StyleId;
+            }
+            else
+            {
+                if (!knownStyles.TryGetValue(name, out style))
+                {
+                    if (!EnsureKnownStyle(name, out style))
+                    {
+                        StyleMissing?.Invoke(this, new StyleEventArgs(name, mainPart, styleType));
+                        return name;
+                    }
+                }
 
-				if (styleType == StyleValues.Character && !style.Type.Equals<StyleValues>(StyleValues.Character))
-				{
-					LinkedStyle linkStyle = style.GetFirstChild<LinkedStyle>();
-					if (linkStyle != null) return linkStyle.Val;
-				}
-				return style.StyleId;
-			}
-		}
+                if (styleType == StyleValues.Character && !style.Type.Equals<StyleValues>(StyleValues.Character))
+                {
+                    LinkedStyle linkStyle = style.GetFirstChild<LinkedStyle>();
+                    if (linkStyle != null) return linkStyle.Val;
+                }
+                return style.StyleId;
+            }
+        }
 
-		#endregion
+        public Style GetKnownStyle(string name, StyleValues styleType = StyleValues.Paragraph, bool ignoreCase = false)
+        {
+            Style style;
 
-		#region DoesStyleExists
+            // OpenXml is case-sensitive but CSS is not.
+            // We will try to find the styles another time with case-insensitive:
+            if (ignoreCase)
+            {
+                if (!knownStyles.TryGetValueIgnoreCase(name, styleType, out style))
+                {
+                    if (StyleMissing != null)
+                    {
+                        StyleMissing(this, new StyleEventArgs(name, mainPart, styleType));
+                        if (knownStyles.TryGetValueIgnoreCase(name, styleType, out style))
+                            return style;
+                    }
+                    return null; // null means we ignore this style (css class)
+                }
 
-		/// <summary>
-		/// Gets whether the given style exists in the document.
-		/// </summary>
-		public bool DoesStyleExists(string name)
-		{
-			return knownStyles.ContainsKey(name);
-		}
+                return style;
+            }
+            else
+            {
+                if (!knownStyles.TryGetValue(name, out style))
+                {
+                    if (!EnsureKnownStyle(name, out style))
+                    {
+                        StyleMissing?.Invoke(this, new StyleEventArgs(name, mainPart, styleType));
+                        return null;
+                    }
+                }
+                return style;
+            }
+        }
 
-		#endregion
+        #endregion
 
-		#region AddStyle
+        #region DoesStyleExists
 
-		/// <summary>
-		/// Add a new style inside the document and refresh the style cache.
-		/// </summary>
-		internal void AddStyle(String name, Style style)
-		{
-			knownStyles[name] = style;
-			if (mainPart.StyleDefinitionsPart == null)
-				mainPart.AddNewPart<StyleDefinitionsPart>().Styles = new Styles();
-			mainPart.StyleDefinitionsPart.Styles.Append(style);
-		}
+        /// <summary>
+        /// Gets whether the given style exists in the document.
+        /// </summary>
+        public bool DoesStyleExists(string name)
+        {
+            return knownStyles.ContainsKey(name);
+        }
 
-		#endregion
+        #endregion
+
+        #region AddStyle
+
+        /// <summary>
+        /// Add a new style inside the document and refresh the style cache.
+        /// </summary>
+        internal void AddStyle(String name, Style style)
+        {
+            knownStyles[name] = style;
+            if (mainPart.StyleDefinitionsPart == null)
+                mainPart.AddNewPart<StyleDefinitionsPart>().Styles = new Styles();
+            mainPart.StyleDefinitionsPart.Styles.Append(style);
+        }
+
+        #endregion
 
         #region EnsureKnownStyle
 
@@ -166,46 +200,46 @@ namespace HtmlToOpenXml
         /// </summary>
         private bool EnsureKnownStyle(string styleName, out Style style)
         {
-			style = null;
-			string xml = PredefinedStyles.GetOuterXml(styleName);
-			if (xml == null) return false;
-			this.AddStyle(styleName, style = new Style(xml));
-			return true;
+            style = null;
+            string xml = PredefinedStyles.GetOuterXml(styleName);
+            if (xml == null) return false;
+            this.AddStyle(styleName, style = new Style(xml));
+            return true;
         }
 
         #endregion
 
-		//____________________________________________________________________
-		//
+        //____________________________________________________________________
+        //
 
-		internal RunStyleCollection Runs
-		{
-			[System.Diagnostics.DebuggerHidden()]
-			get { return runStyle; }
-		}
-		internal TableStyleCollection Tables
-		{
-			[System.Diagnostics.DebuggerHidden()]
-			get { return tableStyle; }
-		}
-		internal ParagraphStyleCollection Paragraph
-		{
-			[System.Diagnostics.DebuggerHidden()]
-			get { return paraStyle; }
-		}
+        internal RunStyleCollection Runs
+        {
+            [System.Diagnostics.DebuggerHidden()]
+            get { return runStyle; }
+        }
+        internal TableStyleCollection Tables
+        {
+            [System.Diagnostics.DebuggerHidden()]
+            get { return tableStyle; }
+        }
+        internal ParagraphStyleCollection Paragraph
+        {
+            [System.Diagnostics.DebuggerHidden()]
+            get { return paraStyle; }
+        }
         internal NumberingListStyleCollection NumberingList
         {
-			// use lazy loading to avoid injecting NumberListDefinition if not required
+            // use lazy loading to avoid injecting NumberListDefinition if not required
             [System.Diagnostics.DebuggerHidden()]
             get { return listStyle ?? (listStyle = new NumberingListStyleCollection(mainPart)); }
         }
 
-		//____________________________________________________________________
-		//
+        //____________________________________________________________________
+        //
 
         /// <summary>
         /// Gets or sets the beginning and ending characters used in the &lt;q&gt; tag.
         /// </summary>
         public QuoteChars QuoteCharacters { get; set; }
-	}
+    }
 }
